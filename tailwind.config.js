665138e16dc84fa5cc7af59ad9a3ce82285b8c04
PavelM <myagkiypavel@gmail.com>
2023-04-14 16:20:19 +0300

    feature: add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,15 @@ module.exports = {
         gold: "#FFD338",
         "jet-black": "#222222",
         "slate-gray": "#555555",
+        champagne: "#FFF9E8",
+        "dim-gray": "#666666",
+        silver: "#999999",
       },
       spacing: {},
       fontSize: {
-        "small": "1rem", // 16px
-        "medium": "1.124rem", // 18px
-        "large": "1.25rem", // 20px
+        small: "1rem", // 16px
+        medium: "1.124rem", // 18px
+        large: "1.25rem", // 20px
         "2xl": "1.375rem", // 22px
         "5xl": "3.25rem", // 52px
         "6xl": "3.5rem", // 56px
